8a76a48852192a388579ad52ea619db5f94215dc
Niels Ouvrard <nielsouvrard@MacBook-Pro-de-Niels.local>
2022-04-01 16:06:46 +0200

    push automatique

--- src/lem_create_nodes.c
+++ src/lem_create_nodes.c
@@ -17,23 +17,21 @@ node_room *new_node_empty (void)
     return node;
 }
 
+void new_tunnel (node_room *source, node_room *destination)
+{
+    source->nmb_tunnels++;
+    ptrs_room *tunnel_room_1 = malloc(sizeof(ptrs_room) * source->nmb_tunnels);
+    for (int i = 0; i < source->nmb_tunnels - 1; i++)
+        tunnel_room_1[i].room = source->tunnel[i].room;
+    tunnel_room_1[source->nmb_tunnels - 1].room = destination;
+    free(source->tunnel);
+    source->tunnel = tunnel_room_1;
+}
+
 void connect_node (node_room *room1, node_room *room2)
 {
-    room1->nmb_tunnels++;
-    ptrs_room *tunnel_room_1 = malloc(sizeof(ptrs_room) * room1->nmb_tunnels);
-    for (int i = 0; i < room1->nmb_tunnels - 1; i++)
-        tunnel_room_1[i].room = room1->tunnel[i].room;
-    tunnel_room_1[room1->nmb_tunnels - 1].room = room2;
-    free(room1->tunnel);
-    room1->tunnel = tunnel_room_1;
-
-    room2->nmb_tunnels++;
-    ptrs_room *tunnel_room_2 = malloc(sizeof(ptrs_room) * room2->nmb_tunnels);
-    for (int i = 0; i < room2->nmb_tunnels - 1; i++)
-        tunnel_room_2[i].room = room2->tunnel[i].room;
-    tunnel_room_2[room2->nmb_tunnels - 1].room = room1;
-    free(room2->tunnel);
-    room2->tunnel = tunnel_room_2;
+    new_tunnel(room1, room2);
+    new_tunnel(room2, room1);
     return;
 }
 
